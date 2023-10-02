988a58d6d2d62338d130c64b1106fc141a53ccd5
Prithvi Rao Ratti <prithvi_rao_ratti@Prithvis-MacBook-Air.local>
2023-10-02 15:22:49 +0530

    Image save in local device dependencies

--- lib/provider/user_place_provider.dart
+++ lib/provider/user_place_provider.dart
@@ -2,12 +2,21 @@ import 'dart:io';
 
 import 'package:favorite_places/model/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as SysPath;
+import 'package:path/path.dart' as path;
 
 class UserPlaceNotifier extends StateNotifier<List<Place>> {
   UserPlaceNotifier() : super([]);
 
-  void addPlace(String title, File? image) {
-    final place = Place(title: title, image: image);
+  Future<void> addPlace(String title, File? image) async {
+    File? copiedImage;
+    if (image != null) {
+      final appDir = await SysPath.getApplicationDocumentsDirectory();
+      final imageName = path.basename(image.path);
+      copiedImage = await image.copy('${appDir.path}/$imageName');
+    }
+
+    final place = Place(title: title, image: copiedImage);
     state = [place, ...state];
   }
 }
--- lib/widget/location_input.dart
+++ lib/widget/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -8,8 +9,52 @@ class LocationInput extends StatefulWidget {
 }
 
 class _LocationInputState extends State<LocationInput> {
+  var _isFetchingLocation = false;
+
+  void _getCurrentLocation() async {
+    Location location = Location();
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+    LocationData locationData;
+    serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+    permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    setState(() {
+      _isFetchingLocation = true;
+    });
+
+    locationData = await location.getLocation();
+
+    setState(() {
+      _isFetchingLocation = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget preview = Text(
+      'No location chosen',
+      style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: Theme.of(context).colorScheme.onBackground,
+          ),
+      textAlign: TextAlign.center,
+    );
+
+    if (_isFetchingLocation) {
+      preview = const CircularProgressIndicator();
+    }
+
     return Column(
       children: [
         Container(
@@ -22,19 +67,13 @@ class _LocationInputState extends State<LocationInput> {
               color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
             ),
           ),
-          child: Text(
-            'No location chosen',
-            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
-                  color: Theme.of(context).colorScheme.onBackground,
-                ),
-            textAlign: TextAlign.center,
-          ),
+          child: preview,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentLocation,
               icon: const Icon(Icons.location_on),
               label: const Text('Current Location'),
             ),
